b0546c7a6b7fd53c26ef0d7a38c154b2e58f19b2
Yanick <tyankee15@gmail.com>
2020-10-08 18:14:18 -0300

    create forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles = Article.all
       end
@@ -6,5 +7,20 @@ class ArticlesController < ApplicationController
     def show
         	@article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end  
+    
+    def create
+        @article = Article.new(
+            @article = Article.new(article_params)
+            @article.save
+        
+            redirect_to article_path(@article)
+        
+        
+
+    end    
 
 end
