ee921400469f24dceeba9ba572be9b85d02ac5ad
Yanick <tyankee15@gmail.com>
2020-10-13 05:07:58 -0300

    Add edit action to update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,14 +11,10 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end  
-    
-    # def create
-    #     @article = Article.new(
-    #         @article = Article.new(article_params)
-    #         @article.save
-        
-    #         redirect_to article_path(@article)
-    # end 
+
+    def edit
+        @article = Article.find(params[:id])
+      end
     
     def create
         @article = Article.new(article_params)
@@ -27,6 +23,16 @@ class ArticlesController < ApplicationController
           redirect_to @article
         else
           render 'new'
+        end
+      end
+
+      def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
         end
       end
     
